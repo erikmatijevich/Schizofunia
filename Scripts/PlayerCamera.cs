2e79efa73d6e33afd25f28e9bf7868e3cd4b3b47
Erik Matijevich <em@purdue.edu>
2016-03-05 19:06:08 -0500

    init

---
+++ Scripts/PlayerCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+
+    private Vector2 velocity;
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public GameObject player;
+    // Use this for initialization
+    void Start()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
